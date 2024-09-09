0b6c2a3085e160dfe83fa0895760108c3a21a106
https://www.linkedin.com/in/luiza-mylena-costa/ <146290275+luizacosta92@users.noreply.github.com>
2024-09-09 16:35:17 -0300

    Adicionando novos dados.js
    Ampliando base de dados.

--- dados.js
+++ dados.js
@@ -1,45 +1,42 @@
-
-//fazendo uma lista para facilitar a inclusão de dados//
-//fazendo um objeto para reunir informações das listas
-//fonte dos dados a seguir https://ojs.brazilianjournals.com.br/ojs/index.php/BJHR/article/view/21768//
 let dados = [
         {
             titulo: "Indução do parto",
             descricao: "A indução do parto é um procedimento médico que estimula o início do trabalho de parto, geralmente através de medicamentos ou métodos mecânicos. Essa prática pode ser indicada em diversas situações, como quando a gestação ultrapassa o prazo previsto, em casos de rompimento prematuro das membranas ou quando a saúde da mãe ou do bebê está em risco.",
             link: "https://portaldeboaspraticas.iff.fiocruz.br/atencao-mulher/inducao-do-trabalho-de-parto-e-indicacoes-de-cesarea/",
-            keywords: [" indução", " parto", " medicamentos", " métodos mecânicos"],
+            tags: [" indução", " parto", " medicamentos", " métodos mecânicos"],
         },
         {
             titulo: "Parto Natural",
             descricao: "O parto natural é o processo fisiológico de nascimento de um bebê, sem o uso de intervenções médicas como cesariana. Ele pode ocorrer de forma espontânea ou ser induzido em algumas situações. O parto natural é geralmente associado a benefícios para a mãe e o bebê, como menor risco de complicações e maior vínculo afetivo.",
             link: "https://issuu.com/editoraground/docs/partoativo_-_guia_pr__tico_para_o_p",
-            keywords: [" parto natural", " fisiológico", " benefícios", " vínculo mãe-bebê"],
+            tags: [" parto natural", " fisiológico", " benefícios", " vínculo mãe-bebê"],
         },
         {
             titulo: "Enfermeira Obstétrica",
             descricao: "A enfermeira obstétrica é uma profissional de saúde especializada em cuidados durante a gestação, parto e puerpério. Ela acompanha a gestante em todas as etapas, oferecendo suporte emocional e técnico, além de realizar procedimentos como monitoramento fetal e assistência no parto.",
             link: "https://portaldeboaspraticas.iff.fiocruz.br/atencao-mulher/principais-questoes-sobre-atuacao-da-enfermagem-obstetrica-na-equipe-multidisciplinar/",
-            keywords: [" enfermeira obstétrica", " cuidados pré-natal", " parto", " puerpério"],
+            tags: [" enfermeira obstétrica", " cuidados pré-natal", " parto", " puerpério"],
         },
         {
             titulo: "Posições para o parto",
             descricao: "A escolha da posição durante o trabalho de parto pode influenciar a progressão e o conforto da gestante. Existem diversas posições que podem ser adotadas, como a posição vertical, de cócoras e de quatro. Cada posição oferece benefícios específicos.",
             link: "https://www.unasus.gov.br/noticia/voce-conhece-recomendacoes-da-oms-para-o-parto-normal#:~:text=%2D%20Liberdade%20de%20posi%C3%A7%C3%A3o%20e%20movimento,uso%20do%20partograma%20da%20OMS.",
-            keywords: [" posições para o parto", " conforto", " progressão do trabalho de parto"],
+            tags: [" posições para o parto", " conforto", " progressão do trabalho de parto"],
         },
         {
             titulo: "Doula",
             descricao: "A doula é uma profissional não médica que oferece apoio emocional e físico à gestante durante a gravidez, o parto e o pós-parto. Ela pode auxiliar na tomada de decisões, fornecer informações e técnicas de relaxamento.",
             link: "https://www.parterassinfronteras.com/post/enfermeira-obstetra-ou-doula",
-            keywords: [" doula", " apoio emocional", " parto", " pós-parto"],
+            tags: [" doula", " apoio emocional", " parto", " pós-parto"],
         },
         {
             titulo: "Aleitamento Materno",
             descricao: "O aleitamento materno é o alimento ideal para o recém-nascido, fornecendo todos os nutrientes necessários para seu crescimento e desenvolvimento. Além disso, ele fortalece o vínculo entre mãe e bebê e oferece diversos benefícios para a saúde de ambos.",
             link: "https://www.saude.gov.br/aleitamento-materno",
-            keywords: [" aleitamento materno ", " nutrição infantil", " vínculo mãe-bebê"],
+            tags: [" aleitamento materno ", " nutrição infantil", " vínculo mãe-bebê"],
         }
     ];
+
 
 
 
